b9e3ab8a1d66de96cca2dc5a3fe214635fcf76f9
kasundraJeet <kasundrajeet29@gmail.com>
2024-12-03 05:56:07 +0530

    added ne ui kit and router

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -1,12 +1,25 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },  
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
