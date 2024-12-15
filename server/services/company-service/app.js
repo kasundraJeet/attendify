354626dded4a87bbdf5f6f17132de96c112f1613
kasundraJeet <kasundrajeet29@gmail.com>
2024-12-15 09:40:58 +0530

    install new desktop app

--- server/services/company-service/app.js
+++ server/services/company-service/app.js
@@ -17,6 +17,14 @@ connectDB();
 
 app.use('/auth', authRouter);
 
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
 const PORT = port || 3000;
 
 app.listen(PORT, () => {
