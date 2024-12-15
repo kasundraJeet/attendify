8a71650bc40792e47538f8d4510d10e9c72661dc
kasundraJeet <kasundrajeet29@gmail.com>
2024-12-15 10:10:43 +0530

    installtion ui

--- desktop/vite.config.ts
+++ desktop/vite.config.ts
@@ -1,18 +1,25 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
-// https://vitejs.dev/config/
 export default defineConfig(async () => ({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },  
   plugins: [vue()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
@@ -25,7 +32,6 @@ export default defineConfig(async () => ({
         }
       : undefined,
     watch: {
-      // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
